bf82787b05d492ccecfeb57aefdfd6bbccfd6289
Leandro J <leandrojoapi@gmail.com>
2021-01-23 14:58:07 -0300

    Implementação REST Full Entidade Cards - GET POST PUT DELETE

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -1,7 +1,139 @@
+const { request, response } = require('express')
 const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+
+    neDB.find({}).exec(function(exception, cards) {
+
+        if (exception) {
+            const sentence = "ERROR ao listar todos os registros de 'cards'!"
+            console.log(sentence, exception)
+
+            response.status(exception.status)
+            response.json({ 'message': sentence })
+        }
+
+        response.status(200)
+        response.json(cards)
+    })
+}
+
+api.findById = (request, response) => {
+
+    const id = request.params.id
+
+    neDB.findOne({ _id: id }).exec(function(exception, card) {
+
+        if (exception) {
+            const sentence = "ERROR ao listar o registro de 'card' requisitado!"
+            console.log(sentence, exception)
+
+            response.status(exception.status)
+            response.json({ 'message': sentence })
+        }
+
+        response.status(200)
+        response.json(card)
+    })
+}
+
+// Consumo desta API:
+// localhost:3000/cards/paginationAndSorting?pageSize=[TamanhoPagina]&itemsSkip=[RegistrosPular]&sortBy=[CampoOrdenação]
+api.findSorted = (request, response) => {
+
+    let sortBy = request.query.sortBy
+    let pageSize = request.query.pageSize
+    let itemsSkip = request.query.itemsSkip
+
+    // Definição de valores padrão
+    if (typeof sortBy === 'undefined') {
+        sortBy = "cardNumber"
+    }
+    if (typeof pageSize === 'undefined') {
+        pageSize = 10
+    }
+    if (typeof itemsSkip === 'undefined') {
+        itemsSkip = 0
+    }
+    
+
+    neDB.find({}).sort({ [sortBy]: 1 }).skip(itemsSkip).limit(pageSize).exec(function (exception, cards) {
+        if (exception) {
+            const sentence = "ERROR ao listar todos os registros de 'cards' de forma ordenada!"
+            console.log(sentence, exception)
+
+            response.status(exception.status)
+            response.json({ 'message': sentence })
+        }
+
+        response.status(200)
+        response.json(cards)
+    });
+}
+
+api.save = (request, response) => {
+
+    const canonical = request.body
+
+    neDB.insert(canonical, function(exception, card) {
+
+        if (exception) {
+            const sentence = "ERROR ao inserir um novo registro de 'card'!"
+            console.log(sentence, exception)
+
+            response.status(exception.status)
+            response.json({'message': sentence})
+        }
+
+        response.status(201)
+        response.json(card)
+    })
+}
+
+api.deleteById = (request, response) => {
+    
+    const id = request.params.id
+
+    neDB.remove({ _id: id }, {}, function(exception, card) {
+        if (exception) {
+            const sentence = "ERROR ao deletar registro de 'card' solicitado!"
+            console.log(sentence, exception)
+
+            response.status(exception.status)
+            response.json({ 'message': sentence})
+        }
+
+        response.status(200)
+        response.json(card)
+    })
+}
+
+api.updateById = (request, response) => {
+    
+    const id = request.params.id
+    const canonical = request.body
+
+    neDB.update({ _id: id }, { 
+        cardNumber: canonical.cardNumber,
+        embossName: canonical.embossName,
+        customerName: canonical.customerName,
+        documentNumber: canonical.documentNumber,
+        motherName: canonical.motherName,
+        address: canonical.address,
+        city: canonical.city
+    }, {}, function (exception, card) {
+            if (exception) {
+                const sentence = "ERROR ao deletar registro de 'card' solicitado!"
+                console.log(sentence, exception)
+
+                response.status(exception.status)
+                response.json({ 'message': sentence })
+            }
+
+            response.status(200)
+            response.json(card)
+    })
 }
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,14 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards/paginationAndSorting')
+        .get(api.findSorted),
+    app.route('/cards')
         .get(api.findAll)
+        .post(api.save),
+    app.route('/cards/:id')
+        .get(api.findById)
+        .delete(api.deleteById)
+        .put(api.updateById)
 
 }

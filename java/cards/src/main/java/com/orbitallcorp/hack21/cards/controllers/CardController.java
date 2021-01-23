420bee2bd169aed38671b59ee39cd0b493ca2d7a
Leandro J <leandrojoapi@gmail.com>
2021-01-23 11:57:07 -0300

    Ajustes e adição de comentarios

--- java/cards/src/main/java/com/orbitallcorp/hack21/cards/controllers/CardController.java
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/controllers/CardController.java
@@ -16,45 +16,70 @@ public class CardController {
     @Autowired
     private CardService cardService;
 
+    // Metodo para salvar novo registro
+    // Rota: localhost:8080/cards
     @PostMapping("/cards")
     public ResponseEntity<Card> save(@RequestBody Card card) {
 
+        // Chama o metodo "Save" do Service, e envia como parametro o "body" recebido da requisição,
+        // armazenando o resultado final em "savedCard"
         Card savedCard = cardService.save((card));
 
+        // Retorna como resposta um feedback do que foi salvo, junto ao status HTTP 201
         return new ResponseEntity(savedCard, HttpStatus.CREATED);
     }
 
+    // Metodo para buscar todos os registros salvos
+    // Rota: localhost:8080/cards
     @GetMapping("/cards")
     public ResponseEntity<List<Card>> findAll() {
 
+        // Chama o metodo "FindAll" do Service, armazenando o retorno na lista "cards"
         List<Card> cards = cardService.findAll();
 
-        return ResponseEntity.ok(cards);
+        // Retorna, junto ao status HTTP 200, todos os registros encontrados
+        return new ResponseEntity(cards, HttpStatus.OK);
     }
 
+    // Metodo para buscar um registro específico, através de seu ID
+    // Rota: localhost:8080/cards/[idDesejado]
     @GetMapping("/cards/{id}")
     public ResponseEntity<Card> findById(@PathVariable String id) {
 
+        // Converte o parametro enviado pela URL de String para Long
         Long userId = Long.parseLong(id);
+        // Chama o metodo "FindById" do Service, armazenando o registro encontrado em "card"
         Card card = cardService.findById(userId);
 
-        return ResponseEntity.ok(card);
+        // Retorna, junto ao status HTTP 200, o registro encontrado
+        return new ResponseEntity(card, HttpStatus.OK);
     }
 
+    // Metodo para deletar um registro específico, através de seu ID
+    // Rota: localhost:8080/cards/[idDesejado]
     @DeleteMapping("/cards/{id}")
     public void deleteById(@PathVariable String id) {
 
+        // Converte o parametro enviado pela URL de String para Long
         Long userId = Long.parseLong(id);
 
+        // Chama o metodo "deleteById" do Service, o deletando do banco de dados
         cardService.deleteById(userId);
+
+        //Por padrão, retorna o status HTTP 204
     }
 
+    // Metodo para atuailizar um registro específico, através de seu ID
+    // Rota: localhost:8080/cards/[idDesejado]
     @PutMapping(("/cards/{id}"))
     public ResponseEntity<Card> updateById(@PathVariable String id, @RequestBody Card newCard) {
 
+        // Converte o parametro enviado pela URL de String para Long
         Long userId = Long.parseLong(id);
+        // Chama o metodo "FindById" do Service, armazenando o registro encontrado em "card"
         Card card = cardService.findById(userId);
 
+        // Substitui os atributos do registro encontrado pelos atributos presente no "body" recebido da requisição
         card.setCardNumber(newCard.getCardNumber());
         card.setEmbossName(newCard.getEmbossName());
         card.setCustomerName(newCard.getCustomerName());
@@ -63,21 +88,31 @@ public class CardController {
         card.setAddress(newCard.getAddress());
         card.setCity(newCard.getCity());
 
+        // Chama o metodo "Save" do Service, que por padrão atualiza os dados de um registro
+        // quando este possui um "id" já cadastrado
+        // Por fim, armazena o resultado final em "savedCard"
         Card savedCard = cardService.save((card));
 
+        // Retorna como resposta um feedback do que foi salvo, junto ao status HTTP 200
         return new ResponseEntity(savedCard, HttpStatus.OK);
     }
 
+    // Metodo para buscar todos os registros de forma paginada e ordenada
     // Consumo desta API:
     // localhost:8080/cards/paginationAndSorting?pageNo=[NumeroPagina]&pageSize=[TamanhoPagina]&sortBy=[CampoOrdenação]
+    // Caso não informado, pageNo por padrão possuirá o valor "0", pageSize o valor "10" e sortBy "customerName"
     @GetMapping("/cards/paginationAndSorting")
     public ResponseEntity<List<Card>> getAllEmployees(
             @RequestParam(defaultValue = "0") Integer pageNo,
             @RequestParam(defaultValue = "10") Integer pageSize,
-            @RequestParam(defaultValue = "cardNumber") String sortBy)
+            @RequestParam(defaultValue = "customerName") String sortBy)
     {
-        List<Card> list = cardService.getAllEmployees(pageNo, pageSize, sortBy);
+        // Chama o metodo "findAllSorted" do Service, buscando todos os registros,
+        // passando como parametro a pagina requisitada, quantos registros irão existir por pagina e
+        // por qual campo será feito a ordenação
+        List<Card> cards = cardService.findAllSorted(pageNo, pageSize, sortBy);
 
-        return new ResponseEntity<List<Card>>(list, HttpStatus.OK);
+        // Retorna todos os registros já ordenados e paginados, junto ao status HTTP 200
+        return new ResponseEntity<List<Card>>(cards, HttpStatus.OK);
     }
 }
--- java/cards/src/main/java/com/orbitallcorp/hack21/cards/services/CardService.java
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/services/CardService.java
@@ -18,36 +18,51 @@ public class CardService {
     @Autowired
     private CardRepository cardRepository;
 
+    // Metodo para salvar um novo registro
     public Card save(Card card) {
+        // Salva o registro recebido e o retorna
         return cardRepository.save((card));
     }
 
+    // Metodo para buscar todos os registros
     public List<Card> findAll() {
         List<Card> cards = new ArrayList<>();
 
+        // Busca todos os registros, onde cada registro encontrado é adicionado à lista "cards"
         cardRepository.findAll().forEach(cards :: add);
 
+        // Retorna a lista "cards" com os registros encontrados
         return cards;
     }
 
+    // Metodo para buscar um registro específico
     public Card findById(Long userId) {
 
+        // Busca o registro utilizando seu "id" e retorna o elemento encontrado
         return cardRepository.findById(userId).get();
     }
 
+    // Metodo para deletar um registro específico
     public void deleteById(Long userId) {
 
+        // Deleta o registro utilizando seu "id"
         cardRepository.deleteById(userId);
     }
 
-    public List<Card> getAllEmployees(Integer pageNo, Integer pageSize, String sortBy)
+    // Metodo para buscar todos os registro de forma ordenada e paginada
+    public List<Card> findAllSorted(Integer pageNo, Integer pageSize, String sortBy)
     {
+        // Realiza uma requisição de paginação, indicando a pagina requisitada,
+        // quantos elementos poderão existir por pagina e por qual campo ocorrerá a ordenação
         Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
 
+        // Busca todos os elementos, de forma paginada e ordenada, e os armazena em "pagedResult"
         Page<Card> pagedResult = cardRepository.findAll(paging);
 
+        // Caso tenha sido encontrado algum conteudo, requisita este conteudo e o retorna
         if(pagedResult.hasContent()) {
             return pagedResult.getContent();
+        // Caso contrario retorna uma lista vazia
         } else {
             return new ArrayList<Card>();
         }
